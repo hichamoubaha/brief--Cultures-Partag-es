38d8c0a881906ed8ff8fae44f81b58ed19c5daef
hichamoubaha <oubahahicham7@gmail.com>
2024-12-31 10:08:15 +0100

    creat table utilisateurs

--- script.sql
+++ script.sql
@@ -3,3 +3,13 @@ CREATE DATABASE art_culture_platform;
 
 -- Sélectionner la base de données
 USE art_culture_platform;
+
+-- Table utilisateurs
+CREATE TABLE utilisateurs (
+    id_utilisateur INT AUTO_INCREMENT PRIMARY KEY,
+    nom_utilisateur VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    mot_de_passe VARCHAR(255) NOT NULL,
+    role ENUM('admin', 'auteur', 'utilisateur') NOT NULL,
+    date_creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
