87dac3f94ed4e815562e4b34d33d07053a96b9ca
hichamoubaha <oubahahicham7@gmail.com>
2024-12-31 10:11:44 +0100

    ajouter les requettes de insertions sur mes tableaux

--- script.sql
+++ script.sql
@@ -34,3 +34,24 @@ CREATE TABLE articles (
     FOREIGN KEY (id_categorie) REFERENCES categories(id_categorie),
     FOREIGN KEY (id_auteur) REFERENCES utilisateurs(id_utilisateur)
 );
+
+-- Insérer des catégories initiales
+INSERT INTO categories (nom_categorie) VALUES
+('Peinture'),
+('Musique'),
+('Littérature'),
+('Cinéma'),
+('Sculpture'),
+('Photographie');
+
+-- Insérer des utilisateurs initiaux
+INSERT INTO utilisateurs (nom_utilisateur, email, mot_de_passe, role) VALUES
+('Admin1', 'admin1@example.com', 'admin1passwordhash', 'admin'),
+('Auteur1', 'auteur1@example.com', 'auteur1passwordhash', 'auteur'),
+('Utilisateur1', 'utilisateur1@example.com', 'utilisateur1passwordhash', 'utilisateur');
+
+-- Insérer des articles initiaux
+INSERT INTO articles (titre, contenu, id_categorie, id_auteur, statut) VALUES
+('Titre Article 1', 'Contenu de l\'Article 1', 1, 2, 'approuve'),
+('Titre Article 2', 'Contenu de l\'Article 2', 2, 2, 'en attente'),
+('Titre Article 3', 'Contenu de l\'Article 3', 3, 2, 'refuse');
